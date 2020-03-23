a2c43c380bc5863072a226c7f3e7327f52b9bee5
fraite <yitlow97>
2020-03-23 20:13:35 +0000

    created __main__.py to allow pyted to be run easily

---
+++ __main__.py
@@ -0,0 +1,5 @@
+from pyted import pyted_runner
+
+if __name__ == '__main__':
+    pyted_runner.run()
+
--- pyted/pyted_runner.py
+++ pyted/pyted_runner.py
@@ -3,5 +3,10 @@ Run this script to start pyted.
 """
 from pyted.pyted_code.pyted_core import PytedCore
 
+
+def run():
+    PytedCore()
+
+
 if __name__ == '__main__':
     x = PytedCore()
